b3337574e12dcb1b90da23491b3893e7274d8551
Nethelbert <gilblasse@gmail.com>
2019-10-18 15:30:56 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,11 @@
 def meal_choice (veg1,veg2,protein="meat")
-  order_message = "What a nutritious meal! A plate of #{protein} with #{veg1} and #{veg2}."
+  nutritious_meal_message ="What a nutritious meal!"
+  order_message = "A plate of #{protein} with #{veg1} and #{veg2}."
+  
+   puts nutritious_meal_message
    puts order_message
-   return order_message
+   
+   return "#{nutritious_meal_message} #{order_message}"
 end
 
 puts meal_choice("broccoli","carrots","steak")
